fffd98bf24bd2df221dbd7d0886054ba663e2191
Tomas Havlicek <havlicek.tomas@gmail.com>
2021-09-17 09:28:31 +0200

    Initial commit

---
+++ SwiftUIExpandableList/SwiftUIExpandableListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExpandableListApp.swift
+//  SwiftUIExpandableList
+//
+//  Created by Tomas Havlicek on 16.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExpandableListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
